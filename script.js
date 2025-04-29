98f12d9a07ef31c5af0d6369775cdde366b39134
HasnatMughal <mughasnat377@gmail.com>
2025-04-29 08:56:35 +0500

    main

---
+++ script.js
@@ -0,0 +1,37 @@
+const nextBtn = document.querySelector('.next');
+const prevBtn = document.querySelector('.prev');
+const slides = document.querySelectorAll('.slide');
+
+let currentSlide = 0;
+
+function Slide(index){
+    if(index >= slides.length){
+        currentSlide = 0;
+    } else if(index < 0){
+currentSlide = slides.length - 1;
+    } else{
+        currentSlide = index
+    }
+
+    const offset = -currentSlide * 100
+    document.querySelector('.slides').style.transform = `translateX(${offset}%)` 
+}
+
+function nextSlide(){
+    Slide(currentSlide + 1)
+}
+
+function prevSlide(){
+    Slide(currentSlide - 1)
+}
+
+nextBtn.addEventListener('click',function(){
+    nextSlide()
+})
+prevBtn.addEventListener('click',function(){
+    prevSlide()
+})
+
+setInterval(()=>{
+    nextSlide()
+},3000)
