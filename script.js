7568bcb8a5b110da8bc1e32d32cd2fdcc438a097
HasnatMughal <mughasnat377@gmail.com>
2025-05-02 21:54:32 +0500

    origin

--- script.js
+++ script.js
@@ -35,3 +35,50 @@ prevBtn.addEventListener('click',function(){
 setInterval(()=>{
     nextSlide()
 },3000)
+
+// ####### Sale Timer #######
+
+const endDate = new Date("April 15,2025 20:59");
+const newDate = new Date(Date.now());
+
+const days = document.querySelectorAll('.days');
+const hours = document.querySelectorAll('.hours');
+const minutes = document.querySelectorAll('.minutes');
+
+const endDay = endDate.getDate();
+const endHours = endDate.getHours();
+const endMinutes = endDate.getMinutes();
+// console.log(newDate);
+
+const todayDate = newDate.getDate();
+const todayHours = newDate.getHours();
+const todayMinutes = newDate.getMinutes();
+// console.log(todayMinutes);
+
+console.log(days)
+
+function changeDate(){
+const finalMinutes = endMinutes - todayMinutes;
+const finalHours = endHours - todayHours;
+const finalDays = endDay - todayDate;
+
+days.forEach((day) => {
+    day.textContent = finalDays;
+})
+hours.forEach((hour) => {
+    hour.textContent = -finalHours;
+})
+minutes.forEach((minute) => {
+    
+    minute.textContent = finalMinutes;
+})
+
+
+console.log(finalDays)
+}
+
+
+
+// console.log(changeDate())
+
+
