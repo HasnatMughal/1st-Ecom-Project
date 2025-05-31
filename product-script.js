5ef9af139c977a3c954d9681955e45cc67cd47ca
HasnatMughal <mughasnat377@gmail.com>
2025-05-31 08:50:53 +0500

    origin

--- product-script.js
+++ product-script.js
@@ -1,210 +1,167 @@
-const jacketBtn = document.querySelector('.jackets');
-const coatBtn = document.querySelector('.coats');
-const trouserBtn = document.querySelector('.trousers');
+const jacketBtn = document.querySelector(".jackets");
+const coatBtn = document.querySelector(".coats");
+const trouserBtn = document.querySelector(".trousers");
 
-const jacketWrap = document.querySelector('.jackets-wrap')
-const coatWrap = document.querySelector('.coats-wrap')
-const trouserWrap = document.querySelector('.trousers-wrap')
+const jacketWrap = document.querySelector(".jackets-wrap");
+const coatWrap = document.querySelector(".coats-wrap");
+const trouserWrap = document.querySelector(".trousers-wrap");
 
-jacketBtn.addEventListener('click',function(){
-jacketWrap.classList.add('active');
-coatWrap.classList.remove('active')
-trouserWrap.classList.remove('active')
-})
+jacketBtn.addEventListener("click", function () {
+  jacketWrap.classList.add("active");
+  coatWrap.classList.remove("active");
+  trouserWrap.classList.remove("active");
+});
 
-coatBtn.addEventListener('click',function(){
-    coatWrap.classList.add('active')
-jacketWrap.classList.remove('active');
-trouserWrap.classList.remove('active')
-})
+coatBtn.addEventListener("click", function () {
+  coatWrap.classList.add("active");
+  jacketWrap.classList.remove("active");
+  trouserWrap.classList.remove("active");
+});
 
-trouserBtn.addEventListener('click',function(){
-    trouserWrap.classList.add('active')
-jacketWrap.classList.remove('active');
-coatWrap.classList.remove('active')
-})
-
+trouserBtn.addEventListener("click", function () {
+  trouserWrap.classList.add("active");
+  jacketWrap.classList.remove("active");
+  coatWrap.classList.remove("active");
+});
 
 // ###############################
 
+const prices = [
+  12.99, 15.99, 10.99, 9.99, 12.99, 15.99, 10.99, 9.99, 12.99, 15.99, 10.99,
+  9.99, 12.99, 15.99, 10.99, 7.99, 4.99, 5.99, 5.99,
+];
 
-
-const prices = [12.99 , 15.99 , 10.99, 9.99 ,12.99 , 15.99 , 10.99, 9.99 ,12.99 , 15.99 , 10.99, 9.99 ,12.99 , 15.99 , 10.99, 7.99 ,4.99 , 5.99 ,5.99];
-
-const price = document.querySelectorAll('.price');
+const price = document.querySelectorAll(".price");
 
 price.forEach((price, index) => {
-price.innerHTML = prices[index]
-})
+  price.innerHTML = prices[index];
+});
 
 const productNames = [
-    "Black Puffer Jacket",
-    "Dark Grey Winter Coat",
-    "Olive Green Hooded Jacket",
-    "Navy Blue Puffer Coat",
-    "Black Bomber Jacket",
-    "Army Green Vest",
-    "Beige Hooded Vest",
-    "Black and White Varsity Jacket",
-    "Grey Pullover Hoodie",
-    "Black Zip-up Jacket",
-    "Black and Grey Checkered Jacket",
-    "Brown Overcoat",
+  "Black Puffer Jacket",
+  "Dark Grey Winter Coat",
+  "Olive Green Hooded Jacket",
+  "Navy Blue Puffer Coat",
+  "Black Bomber Jacket",
+  "Army Green Vest",
+  "Beige Hooded Vest",
+  "Black and White Varsity Jacket",
+  "Grey Pullover Hoodie",
+  "Black Zip-up Jacket",
+  "Black and Grey Checkered Jacket",
+  "Brown Overcoat",
   "Black Overcoat",
   "Red Overcoat",
   "Beige Overcoat",
   "Black Joggers",
   "Navy Blue Joggers",
   "Gray Joggers",
-  "Charcoal Black Joggers"
-  ];
+  "Charcoal Black Joggers",
+];
 
-  const productName =
-   document.querySelectorAll('.product-name');
-   
-  productName.forEach((product, index) => {
-product.innerHTML = productNames[index]
+const productName = document.querySelectorAll(".product-name");
 
-  })
+productName.forEach((product, index) => {
+  product.innerHTML = productNames[index];
+});
 
 // ############################
-const cart = document.querySelector('.cart')
+const cart = document.querySelector(".cart");
 
-const cartBtn = document.querySelector('.cart_btn');
-cartBtn.addEventListener('click',function(){
-  cart.classList.toggle('active')
-})
+const cartBtn = document.querySelector(".cart_btn");
+cartBtn.addEventListener("click", function () {
+  cart.classList.toggle("active");
+});
 
-
-
-
-
-
-const cartWrap = document.querySelector('.cart-products-wrap');
-
-
-let cartData = [];
-function addCartProduct(name,price ){
-    
-    let cartProductName = document.createElement('p');
-    cartProductName.className = 'cart-product-name';
-   
-    let productPrice = document.createElement('p');
-    productPrice.className = 'cart-product-price';
-
-    let removeBtn = document.createElement('button');
-    removeBtn.className = 'remove-btn'
-   
-    
-
-      let cartDescDiv = document.createElement('div');
-    cartDescDiv.className ='cart-product-desc';
-    cartDescDiv.appendChild(cartProductName);
-    cartDescDiv.appendChild(productPrice);
-
-    let cartProduct = document.createElement('div');
-    cartProduct.className = 'cart-product';
-    cartProduct.appendChild(cartDescDiv)
-    cartProduct.appendChild(removeBtn);
-
-     removeBtn.textContent = "Remove";
-
-
-
-       cartProductName.textContent = `${name}` 
-productPrice.textContent = `${price}` + '$'
-
-    cartWrap.appendChild(cartProduct);
-    
- 
-}
-
-function addPrice(){
-
-}
-
-let Product = '';
-let ProductnameForCart = '';
-let ProductpriceForCart = null;
+const cartWrap = document.querySelector(".cart-products-wrap");
 
 let itemNo = 0;
+const productItemNo = document.querySelector(".cart-item-num");
 
-const addToCartBtn = document.querySelectorAll('.cart-btn');
+let total = 0;
+const totalAmount = document.querySelector(".total-amount");
 
-addToCartBtn.forEach(btn => btn.addEventListener('click',(event) => {
-const product = event.target.closest('.product');
-const productNameForCart = product.querySelector('.product-name').textContent;
-const productPriceForCart = product.querySelector('.price').textContent;
+let cartData = [];
+function addCartProduct(name, price) {
+  let cartProductName = document.createElement("p");
+  cartProductName.className = "cart-product-name";
 
-addCartProduct(productNameForCart, productPriceForCart);
+  let productPrice = document.createElement("p");
+  productPrice.className = "cart-product-price";
 
+  let removeBtn = document.createElement("button");
+  removeBtn.className = "remove-btn";
 
-// #################################
+  let cartDescDiv = document.createElement("div");
+  cartDescDiv.className = "cart-product-desc";
+  cartDescDiv.appendChild(cartProductName);
+  cartDescDiv.appendChild(productPrice);
 
-let incr = ++itemNo ;
+  let cartProduct = document.createElement("div");
+  cartProduct.className = "cart-product";
+  cartProduct.appendChild(cartDescDiv);
+  cartProduct.appendChild(removeBtn);
 
-const productItemNo = document.querySelector('.cart-item-num');
-productItemNo.textContent = incr;
+  removeBtn.textContent = "Remove";
 
+  cartProductName.textContent = `${name}`;
+  productPrice.textContent = `${price}` + "$";
 
-// ###########################
+  cartWrap.appendChild(cartProduct);
 
-   let total = 0;
-const cartProductPrices = cartWrap.querySelectorAll('.cart-product-price');
-
-cartProductPrices.forEach(priceElement => {
-  const iop = parseFloat(priceElement.textContent)
-  
-  
-  total += iop;
-  
-  const totalAmount = document.querySelector('.total-amount');
-  totalAmount.innerHTML = total + '$';
-  
-})
-
-function saveToCart (product){
-
+  removeBtn.addEventListener("click", function () {
+    cartProduct.remove();
+    let decr = --itemNo;
+    productItemNo.textContent = decr;
+    const removedPrice = parseFloat(productPrice.textContent);
+    total -= removedPrice;
+    totalAmount.innerHTML = total.toFixed(2) + "$";
+  });
 }
 
+const addToCartBtn = document.querySelectorAll(".cart-btn");
 
-// prices.forEach(priceElement => {
-//   total += parseFloat(console.log(priceElement.textContent))
-// })
+addToCartBtn.forEach((btn) =>
+  btn.addEventListener("click", (event) => {
+    const product = event.target.closest(".product");
+    const productNameForCart =
+      product.querySelector(".product-name").textContent;
+    const productPriceForCart = product.querySelector(".price").textContent;
 
+    addCartProduct(productNameForCart, productPriceForCart);
 
+    // #################################
 
+    let incr = ++itemNo;
+    productItemNo.textContent = incr;
+    // ##########################
 
+    const priceInCart = parseFloat(productPriceForCart);
+    console.log(priceInCart);
 
+    total += priceInCart;
 
-// const calculateBtn = document.querySelector('.')
-
-return {
-  product,
-  productNameForCart,
-  productPriceForCart
-}
-
-
-}))
-// console.log(cartData);
-
-
-
-
-
+    totalAmount.innerHTML = total + "$";
+  })
+);
 
 // Notes and mistakes
 // const productElements = document.querySelectorAll('.product');
 // console.log(productElements);
 // productElements.forEach(element => {
 //   element.addEventListener('click',function(){
-//   console.log(element.querySelector('.product-name').textContent)  
+//   console.log(element.querySelector('.product-name').textContent)
 //   })
 // productElements.forEach(element => {
 //   console.log(element.querySelector('.product-name'))
 // })
 // productElements.forEach(element => {
 //   console.log(element.querySelector('.price'))
-  
+
 // })
+
+// prices.forEach(priceElement => {
+//   total += parseFloat(console.log(priceElement.textContent))
+// })
+
+// const calculateBtn = document.querySelector('.')
